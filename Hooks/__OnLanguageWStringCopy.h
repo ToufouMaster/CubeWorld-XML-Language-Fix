7be96cfb9949531e16726e4d25d1eee4f1fbffe5
ToufouMaster <owen.kala@gmx.fr>
2025-04-20 13:03:22 +0200

    Code cleanup

--- Hooks/__OnLanguageWStringCopy.h
+++ Hooks/__OnLanguageWStringCopy.h
@@ -9,34 +9,37 @@ void* ASMOnLanguageWStringCopy_jmpback;
 
 sqlite3* db;
 std::vector<std::string> file_names = {};
-const char* current_lang_file = "";
 
 int last_id = 0;
+
+void LoadLanguageXMLFile(cube::GameController* gc, std::string* string) {
+    auto base = cube_funcs::instance()->ImageBase;
+    auto addr = (uint32_t)string->c_str();
+
+    int a = (addr >> 24) & 0xff;
+    int b = (addr >> 16) & 0xff;
+    int c = (addr >> 8) & 0xff;
+    int d = addr & 0xff;
+
+    // set bytes to
+    // push [string]
+    WriteBYTE((void*)(base + 0xE1B3E), 0x68);
+    WriteBYTE((void*)(base + 0xE1B3F), d);
+    WriteBYTE((void*)(base + 0xE1B40), c);
+    WriteBYTE((void*)(base + 0xE1B41), b);
+    WriteBYTE((void*)(base + 0xE1B42), a);
+
+    typedef cube::Speech* (__fastcall* load_language_t)(cube::Speech* speech);
+    load_language_t load_language = (load_language_t)(cube_funcs::instance()->ImageBase + 0xE1970);
+    load_language(&gc->world.speech);
+}
 
 std::wostream* __fastcall LanguageWStringCopy(std::wostream* stream) {//cube::OptionsWidget* widget) {
     auto gc = cube::GetGameController();
     int id = gc->OptionsWidget->language_id % file_names.size();
-    current_lang_file = file_names.at(id).c_str();
 
     if (id != last_id) {
-        // Call function
-        typedef cube::Speech* (__fastcall* load_language_t)(cube::Speech* speech);
-        load_language_t load_language = (load_language_t)(cube_funcs::instance()->ImageBase + 0xE1970);
-
-        auto base = cube_funcs::instance()->ImageBase;
-        auto the_int = (uint32_t)current_lang_file;
-
-        int a = (the_int >> 24) & 0xff;
-        int b = (the_int >> 16) & 0xff;
-        int c = (the_int >> 8) & 0xff;
-        int d = the_int & 0xff;
-
-        WriteBYTE((void*)(base + 0xE1B3F), d);
-        WriteBYTE((void*)(base + 0xE1B40), c);
-        WriteBYTE((void*)(base + 0xE1B41), b);
-        WriteBYTE((void*)(base + 0xE1B42), a);
-
-        load_language(&gc->world.speech);
+        LoadLanguageXMLFile(gc, &file_names.at(id));
         last_id = id;
     }
     //stream->write(std::to_wstring(id).c_str(), std::to_wstring(id).size());
@@ -85,7 +88,6 @@ void SetupOnLanguageWStringCopy() {
     auto gc = cube::BusywaitForGameController();
     int id = gc->option_language % file_names.size();
     last_id = id;
-    current_lang_file = file_names.at(id).c_str();
 
     auto base = cube_funcs::instance()->ImageBase;
     WriteBYTE((void*)(base + 0xD369D), 0x90);
@@ -106,22 +108,7 @@ void SetupOnLanguageWStringCopy() {
     WriteCALL((void*)(base + 0xD36A9), (void*)&LanguageWStringCopy);
     WriteBYTE((void*)(base + 0xD36AE), 0x90);
 
-    auto the_int = (uint32_t)current_lang_file;
-
-    int a = (the_int >> 24) & 0xff;  
-    int b = (the_int >> 16) & 0xff;  
-    int c = (the_int >> 8) & 0xff;  
-    int d = the_int & 0xff;
-
-    WriteBYTE((void*)(base + 0xE1B3E), 0x68);
-    WriteBYTE((void*)(base + 0xE1B3F), d);
-    WriteBYTE((void*)(base + 0xE1B40), c);
-    WriteBYTE((void*)(base + 0xE1B41), b);
-    WriteBYTE((void*)(base + 0xE1B42), a);
-    
-    typedef cube::Speech* (__fastcall* load_language_t)(cube::Speech* speech);
-    load_language_t load_language = (load_language_t)(cube_funcs::instance()->ImageBase + 0xE1970);
-    load_language(&gc->world.speech);
+    LoadLanguageXMLFile(gc, &file_names.at(id));
 
     ASMOnLanguageWStringCopy_jmpback = (void*)(base + 0xD36AF);
 }
